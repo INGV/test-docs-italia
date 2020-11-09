632f7bf6a5668770a7324182802cebddd8ad77cb
robertovallone <roberto.vallone@ingv.it>
2020-11-09 16:14:47 +0000

    2nd version INGV customization draft

--- conf.py
+++ conf.py
@@ -66,9 +66,9 @@ html_theme = 'docs_italia_theme'
 html_theme_path = [docs_italia_theme.get_html_theme_path()]
 html_title = "test-docs-italia"
 html_show_sourcelink = False
-html_favicon = "favicon.ico"
-html_logo = "logo.png"
-latex_logo = 'logo.png'
+html_favicon = "docs-italia-ingv-theme-template/static/images/favicon.ico"
+html_logo = "docs-italia-ingv-theme-template/static/images/logo_75_transp.png"
+latex_logo = 'docs-italia-ingv-theme-template/static/images/logo_75_transp.png'
 html_baseurl = 'docs'
 smartquotes = False
 language = "it"
@@ -81,4 +81,4 @@ source_suffix = '.rst'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+html_static_path = ['docs-italia-ingv-theme-template/static']
